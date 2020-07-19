71d0aa399d21542f1cfe1ac1468d5b792c25aa2d
georgina <georginachew@hotmail.sg>
2020-07-19 10:16:56 +0800

    changed location of pokemon class codes

--- PokeSim/Pokemon.cs
+++ PokeSim/Pokemon.cs
@@ -5,6 +5,38 @@ using System;
 
 namespace PokeSim {
     // TODO for Part 1
+     class Pokemon
+    {
+        private string name;
+        private int hp;
+        private double exp;
+
+        public Pokemon(string name)
+        {
+            this.name = name;
+            this.hp = 100;
+            this.exp = 0;
+        }
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int Hp
+        {
+            get { return this.hp; }
+            set { this.hp = value; }
+        }
+        public double Exp
+        {
+            get { return this.exp; }
+            set { this.exp = value; }
+        }
+        public virtual void Evolve()
+        {
+            Console.WriteLine("");
+        }
+    }
     interface Normal {
         public void Runaway();
     }
